f163905e43c6bef60409a43d048094ed6102f71b
Artem <167628987+ArtemZinukov@users.noreply.github.com>
2026-05-16 19:56:08 +0300

    Добавил кастомный форматтер, чтобы подставлять N/A в ticket_id, если тот не найден

--- app/core/logging.py
+++ app/core/logging.py
@@ -2,12 +2,22 @@ import logging
 from typing import Any, MutableMapping
 
 
+class OptionalTicketFormatter(logging.Formatter):
+    """Кастомный форматтер. Если у лога нет ticket_id (вызов без адаптера),
+    он автоматически подставит 'N/A'."""
+
+    def format(self, record: logging.LogRecord) -> str:
+        if not hasattr(record, "ticket_id"):
+            record.ticket_id = "N/A"
+        return super().format(record)
+
+
 def get_logger(name: str = "MAS_Support") -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
         logger.setLevel(logging.INFO)
         handler = logging.StreamHandler()
-        formatter = logging.Formatter("%(asctime)s - [%(levelname)s] - Ticket: %(ticket_id)s - %(message)s")
+        formatter = OptionalTicketFormatter("%(asctime)s - [%(levelname)s] - Ticket: %(ticket_id)s - %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
     return logger
@@ -18,4 +28,9 @@ class TicketLoggerAdapter(logging.LoggerAdapter[logging.Logger]):
         extra_dict = self.extra if self.extra is not None else {}
         ticket_id = extra_dict.get("ticket_id", "UNKNOWN")
 
-        return msg, {**kwargs, "extra": {"ticket_id": ticket_id}}
+        if "extra" not in kwargs:
+            kwargs["extra"] = {}
+
+        kwargs["extra"]["ticket_id"] = ticket_id
+
+        return msg, kwargs
